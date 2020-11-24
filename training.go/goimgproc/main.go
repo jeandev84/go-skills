c78eaf660347fb1700fa83a128a2ddd5fbba48b9
Yao Jean-Claude <jeandev84@yahoo.com>
2020-11-24 04:19:45 +0300

    cli

--- training.go/goimgproc/main.go
+++ training.go/goimgproc/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
@@ -12,10 +13,26 @@ import (
 // $ go run main.go
 func main() {
 
+	var srcDir = flag.String("src", "", "Input Directory")
+	var dstDir = flag.String("dst", "", "Output Directory")
+	var filterType = flag.String("filter", "grayscale", "grayscale/blur")
+	flag.Parse()
+
 	var f filter.Filter = filter.Grayscale{}
 
-	// t := task.NewWaitGrpTask("./imgs", "output", f)
-	t := task.NewChanTask("./imgs", "output", f, 4)
+	switch *filterType {
+	case "grayscale":
+		f = filter.Grayscale{}
+	case "blur":
+		f = filter.Blur{}
+	}
+
+	/*
+		t := task.NewWaitGrpTask("./imgs", "output", f)
+		t := task.NewChanTask("./imgs", "output", f, 4)
+	*/
+
+	t := task.NewChanTask(*srcDir, *dstDir, f, 16)
 
 	start := time.Now() // debut du temps du process
 	t.Process()

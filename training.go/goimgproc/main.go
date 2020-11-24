6ec6b14092239411c9428943a53a9b0c993d9214
Yao Jean-Claude <jeandev84@yahoo.com>
2020-11-24 04:33:42 +0300

    cli task

--- training.go/goimgproc/main.go
+++ training.go/goimgproc/main.go
@@ -11,11 +11,17 @@ import (
 
 // Entry point
 // $ go run main.go
+/*
+$ go run main.go -src imgs/ -dst output -filter blur -task waitgrp
+*/
 func main() {
 
 	var srcDir = flag.String("src", "", "Input Directory")
 	var dstDir = flag.String("dst", "", "Output Directory")
 	var filterType = flag.String("filter", "grayscale", "grayscale/blur")
+	var taskType = flag.String("task", "waitgrp", "waitgrp/channel")
+	var poolSize = flag.Int("poolsize", 4, "Workers pool size for the channel task")
+
 	flag.Parse()
 
 	var f filter.Filter = filter.Grayscale{}
@@ -25,14 +31,24 @@ func main() {
 		f = filter.Grayscale{}
 	case "blur":
 		f = filter.Blur{}
+		/*default:
+		fmt.Println("Filter not found")*/
 	}
 
 	/*
 		t := task.NewWaitGrpTask("./imgs", "output", f)
 		t := task.NewChanTask("./imgs", "output", f, 4)
+		t := task.NewChanTask(*srcDir, *dstDir, f, 16)
 	*/
 
-	t := task.NewChanTask(*srcDir, *dstDir, f, 16)
+	var t task.Tasker
+
+	switch *taskType {
+	case "waitgrp":
+		t = task.NewWaitGrpTask(*srcDir, *dstDir, f)
+	case "channel":
+		t = task.NewChanTask(*srcDir, *dstDir, f, *poolSize)
+	}
 
 	start := time.Now() // debut du temps du process
 	t.Process()
